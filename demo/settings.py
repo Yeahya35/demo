3f9d74221fbe34197e9532c6cda4463ab9d04fce
Yeahya35 <yahyaeren35@hotmail.com>
2022-10-01 15:44:24 +0300

    crud operations

--- demo/settings.py
+++ demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yxsng@84v8(&e&48!spd+usk&%mach&%+u-p5-yri=n-btjgc7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Istanbul'
 
 USE_I18N = True
 
@@ -119,8 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from operator import methodcaller
+from django.shortcuts import render, redirect
 from .models import Project
 from .forms import ProjectForm
 
@@ -15,6 +16,31 @@ def project(request, pk):
 
 
 def createProject(request):
-    form = ProjectForm()
+    form = ProjectForm(request.POST, request.FILES)
+    if form.is_valid():
+        form.save()
+        return redirect('projects')
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
+
+
+def updateProject(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    form = ProjectForm(instance=projectObj)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES, instance=projectObj)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
+
+
+def deleteProject(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        projectObj.delete()
+        return redirect('projects')
+    context = {'object': projectObj}
+    return render(request, 'projects/delete_template.html', context)
